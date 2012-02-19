e64d7ce7cc5d8c1eecd741242309930e9a9279f9
Lucas Wilson-Richter <da.maestro@gmail.com>
2012-02-19 16:55:43 +1100

    Bug fix

--- scripts/config/torrent_processor.config.rb
+++ scripts/config/torrent_processor.config.rb
@@ -3,5 +3,5 @@ FtpRoot = "ftp://grabbit/Downloads"
 FtpFSRootRegex = /\/path\/to\/Downloads\//
 FtpReadyTag = 'ftp_tag_name'
 
-TransmissionConfig = {"url" => "http://transmission:9091/transmission/rpc", "username" => "transmission", "password" => "n8Xic6RS"}
+TransmissionConfig = {"url" => "http://transmission:9091/transmission/rpc", "username" => "transmission", "password" => "password123"}
 
