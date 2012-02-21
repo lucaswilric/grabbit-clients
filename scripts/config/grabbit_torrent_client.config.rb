025dad85186308a27cf9bc9cfc25bbad18da012d
Lucas Wilson-Richter <da.maestro@gmail.com>
2012-02-21 22:53:38 +1100

    Fixed missing config setting

--- scripts/config/grabbit_torrent_client.config.rb
+++ scripts/config/grabbit_torrent_client.config.rb
@@ -1,6 +1,6 @@
 GrabbitUrl = "http://localhost:3000"
 GrabbitTag = "tag_name"
-
+DestinationRoot = "/path/to/destination"
 TransmissionConfig = {"url" => "http://transmission:9091/transmission/rpc", "username" => "transmission", "password" => "password123"}
 
 
