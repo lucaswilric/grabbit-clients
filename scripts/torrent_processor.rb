a7df7142e84fdc7d9a1e2d8aaa69765208d3c057
Lucas Wilson-Richter <da.maestro@gmail.com>
2012-05-16 08:00:07 +1000

    Fixed bug in torrent processor

--- scripts/torrent_processor.rb
+++ scripts/torrent_processor.rb
@@ -40,7 +40,7 @@ class TorrentProcessor
         
         # Send a Jabber message to say the download is finished.
         im = Jabber::Simple.new(JabberConfig["id"], JabberConfig["password"])
-        JabberConfig["recipients"].each {|r| im.deliver(r, "#{file.name} finished downloading.") }
+        JabberConfig["recipients"].each {|r| im.deliver(r, "#{file['name']} finished downloading.") }
       elsif r['errors'] == {"base" => ["This URL is already waiting to be downloaded"]}
         # Raise unless we get success back, or the error message expected.
         puts "Job for '#{ftp_url_for_file(file, dir)}' already exists"
